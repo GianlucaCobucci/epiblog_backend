5daa7a6e0ba3c657b065c25e6e26a531a8b1614f
GianlucaCobucci <gianlucacobucci330@gmail.com>
2023-06-14 12:28:47 +0200

    cookie session

--- routes/gitHubAuth.js
+++ routes/gitHubAuth.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import passport from 'passport'
 import { Strategy as GitHubStrategy } from 'passport-github2';
-import session from 'express-session'
+import session from 'cookie-session'
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import jwt from 'jsonwebtoken';
 
 dotenv.config();
 
@@ -11,25 +12,24 @@ const router = express.Router()
 
 router.use(cookieParser())
 
-router.use(
-    session({
-        secret: process.env.GITHUB_CLIENT_SECRET,
-        resave: false, 
-        saveUninitialized: false
-    })
+router.use(session({
+    secret: process.env.GITHUB_CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: false
+})
 )
 
 router.use(passport.initialize())
 router.use(passport.session())
 
-passport.serializeUser((user, done)=>{
+passport.serializeUser((user, done) => {
     console.log(user)
-    done(null,user)
+    done(null, user)
 })
 
-passport.deserializeUser((user, done)=>{
+passport.deserializeUser((user, done) => {
     console.log(user)
-    done(null,user)
+    done(null, user)
 })
 
 passport.use(
@@ -37,36 +37,30 @@ passport.use(
         clientID: process.env.GITHUB_CLIENT_ID,
         clientSecret: process.env.GITHUB_CLIENT_SECRET,
         callbackURL: process.env.GITHUB_CALLBACK_URL
-    }, (accessToken, refreshToken, profile, done)=>{
+    }, (accessToken, refreshToken, profile, done) => {
         console.log(profile, accessToken, refreshToken)
         return done(null, profile)
     })
 )
 
-router.get('/auth/github', passport.authenticate('github', {scope: ['user:email']}), (req, res)=>{
+router.get('/auth/github', passport.authenticate('github', { scope: ['user:email'] }), (req, res) => {
     console.log(req.user)
     res.send(req.user)
 })
 
-router.get('/auth/github/callback', passport. authenticate('github', {failureRedirect: '/'}),(req, res)=>{
-    res.redirect('/success')
+router.get('/auth/github/callback', passport.authenticate('github', { failureRedirect: '/' }), (req, res) => {
+    const user = req.user;
+
+    const token = jwt.sign(user, process.env.SECRET_JWT_KEY);
+    const redirectUrl = `http://localhost:3000/success?token=${encodeURIComponent(token)}`;
+    res.redirect(redirectUrl)
 })
 
-router.get('/success', (req, res)=>{
-    if (req.user){
-        const username = req.user.username;
-        console.log(`Benvenuto ${username}`)
-    } else {
-        console.log("Per favore, loggati prima")
-    }
-
-    res.redirect('http://localhost:3000/homepage')
-})
-router.get('/decode-cookie', (req, res) =>{
+router.get('/decode-cookie', (req, res) => {
     const cookie = req.cookies['connect.sid']
     const decodedCookie = decodeURIComponent(cookie)
 
     res.send(decodedCookie)
 })
 
-export default  router
+export default router
